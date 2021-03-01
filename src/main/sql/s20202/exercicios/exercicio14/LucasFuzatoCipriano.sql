29276a9a67b42f8b568e51923633993dfcaf66fb
LucasBR96 <lucas_cipriano@id.uff.br>
2021-03-01 11:47:25 -0300

    exercicio14 feito

--- src/main/sql/s20202/exercicios/exercicio14/LucasFuzatoCipriano.sql
+++ src/main/sql/s20202/exercicios/exercicio14/LucasFuzatoCipriano.sql
@@ -1,5 +1,4 @@
-create or replace function lig_médias( date , date ) returns
-table ( cidade text , bairro text , dur_med float ) as $$
+create or replace function lig_medias( date , date ) returns table( cidade text , bairro text , dur_med float ) as $$
 declare
     t_inicial alias for $1;
     t_final alias for $2;
@@ -20,13 +19,13 @@ begin
     )
     ,tab2 as(
         select
-            ligacao,
+            ligacao_id,
             antena_orig as "ant_num",
             duracao
         from tab1
         union
         select
-            ligacao,
+            ligacao_id,
             antena_dest as "ant_num",
             duracao
         from tab1
@@ -55,7 +54,7 @@ begin
             bairro_nome,
             duracao
         from
-            tab3 inner join tab2 on tab3.antena_id = tab2.antena_num
+            tab3 inner join tab2 on tab3.antena_id = tab2.ant_num
     )
     select
         cidade_nome as "cidade",
@@ -64,6 +63,31 @@ begin
     from
         tab4
     group by ( cidade_nome , bairro_nome )
-    order by dur_med;
-    return
+    order by dur_med desc ; 
+    return;
 end;
+$$ language plpgsql;
+
+do $$
+declare
+    a date := '03/04/2019'::date;
+    b date := '01/10/2019'::date;
+    
+    curs1 refcursor;
+    rec record;
+begin
+    
+    open curs1 for select * from lig_medias( a , b );
+    loop
+        fetch curs1 into rec;
+        exit when not found;
+        raise notice '%, %, %', rec.cidade, rec.bairro, rec.dur_med;
+    end loop;
+    close curs1;
+end;
+$$ language plpgsql;
+
+DO $$ BEGIN
+    PERFORM drop_functions();
+    PERFORM drop_tables();
+END $$;        
